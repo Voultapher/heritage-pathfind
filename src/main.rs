0696c7abac84cd4eea3be3267ba99c3ff670f851
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2019-04-24 22:15:08 +0200

    Store relationship as edge weight

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::collections::HashMap;
 use std::error::Error;
+use std::fmt;
 use std::fs::File;
 use std::io;
 
 use fxhash::FxBuildHasher;
 
 use petgraph::algo::{astar};
-use petgraph::graph::{Graph, NodeIndex};
+use petgraph::graph::{Graph, EdgeIndex, NodeIndex};
 
 use serde::Deserialize;
 
@@ -14,56 +15,87 @@ use structopt::StructOpt;
 
 #[allow(non_snake_case)]
 #[derive(Debug, Deserialize)]
-struct Relationship {
+struct Person {
     PersonID: i32,
     SpouseID: Option<i32>,
     FatherID: Option<i32>,
     MotherID: Option<i32>,
 
-    //PersonID: String,
+    // PersonID: String,
+    /// Name of the person.
     Person: String,
     // SpouseID: String,
     // Ehepartner: String,
-    //FatherID: String,
+    // FatherID: String,
     // Vater: String,
-    //MotherID: String,
+    // MotherID: String,
     // Mutter: String,
     // ChildID: String,
     // Kind: String,
     // RelID: String,
-    Beziehung: String,
-    //RelationKey: String,
+    // Beziehung: String,
+    // RelationKey: String,
 }
+
 
 #[derive(Debug)]
 struct Heritage {
-    relationship: Relationship,
+    person: Person,
     node_idx: NodeIndex<u32>,
 }
 
+
+#[derive(Copy, Clone, Debug)]
+enum Relationship {
+    Spouse,
+    Father,
+    Mother,
+}
+
+
+#[derive(Debug)]
+struct PersonRelationship {
+    id: i32,
+    name: String,
+    relationship: Option<Relationship>,
+}
+
+
 type HeritageMap = HashMap<i32, Heritage, FxBuildHasher>;
 
-// Store person id per node, edges need no information.
+// Store person id per node, and relationship type as edge information.
 // Undirected to allow for indirect heritage paths.
 // u32 index space, if you have more than 4B nodes change.
-type PersonGraph = Graph<i32, (), petgraph::Undirected, u32>;
+type PersonGraph = Graph<i32, Relationship, petgraph::Undirected, u32>;
 
 
-fn add_relationships(
+impl fmt::Display for PersonRelationship {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &self.relationship {
+            Some(rel) => {
+                write!(f, "-> {}({}) is {:?} of", self.name, self.id, rel)
+            },
+            None => { write!(f, "-> {}({})", self.name, self.id) }
+        }
+    }
+}
+
+
+fn add_persons(
     heritage: &mut Heritage,
-    relationship: &Relationship
+    person: &Person
 ) {
     // TODO find a better way to merge structs holding multiple Options.
-    if let Some(_) = relationship.SpouseID {
-        heritage.relationship.SpouseID = relationship.SpouseID;
+    if let Some(_) = person.SpouseID {
+        heritage.person.SpouseID = person.SpouseID;
     }
 
-    if let Some(_) = relationship.FatherID {
-        heritage.relationship.FatherID = relationship.FatherID;
+    if let Some(_) = person.FatherID {
+        heritage.person.FatherID = person.FatherID;
     }
 
-    if let Some(_) = relationship.MotherID {
-        heritage.relationship.MotherID = relationship.MotherID;
+    if let Some(_) = person.MotherID {
+        heritage.person.MotherID = person.MotherID;
     }
 }
 
@@ -81,16 +113,16 @@ fn extract_graph_from_csv<R: io::Read>(
     let mut heritage_map = HeritageMap::default();
 
     for result in rdr.deserialize() {
-        let relationship: Relationship = result?;
-        let person_id: i32 = relationship.PersonID;
+        let person: Person = result?;
+        let person_id: i32 = person.PersonID;
 
         match heritage_map.get_mut(&person_id) {
             Some(mut heritage) => {
-                add_relationships(&mut heritage, &relationship);
+                add_persons(&mut heritage, &person);
             }
             None => {
                 let heritage = Heritage{
-                    relationship: relationship,
+                    person: person,
                     node_idx: graph.add_node(person_id),
                 };
 
@@ -105,64 +137,103 @@ fn extract_graph_from_csv<R: io::Read>(
 
 fn add_graph_edges(graph: &mut PersonGraph, heritage_map: &HeritageMap) {
     for (_, heritage) in heritage_map {
-        let mut add_optional_relationship = |relative_opt| {
+        let mut add_optional_person = |relative_opt, rel| {
             if let Some(relative_id) = relative_opt {
                 if let Some(relative) = heritage_map.get(&relative_id) {
-                    graph.add_edge(heritage.node_idx, relative.node_idx, ());
+                    graph.add_edge(heritage.node_idx, relative.node_idx, rel);
                 }
             }
         };
 
-        add_optional_relationship(heritage.relationship.SpouseID);
-        add_optional_relationship(heritage.relationship.FatherID);
-        add_optional_relationship(heritage.relationship.MotherID);
+        add_optional_person(heritage.person.SpouseID, Relationship::Spouse);
+        add_optional_person(heritage.person.FatherID, Relationship::Father);
+        add_optional_person(heritage.person.MotherID, Relationship::Mother);
     }
+}
+
+
+// Pulls node + optional edge information from node indices.
+// Imo this should be library feature.
+fn map_edges(
+    nodes: &Vec<NodeIndex<u32>>,
+    graph: &PersonGraph
+) -> Vec<(i32, Option<EdgeIndex<u32>>)> {
+    let mut indicies = nodes.iter().rev().peekable();
+    let mut vec = Vec::new();
+
+    loop {
+        if let Some(a) = indicies.next() {
+            let edge_opt = indicies.peek()
+                .map(|b| { graph.find_edge(*a, **b) })
+                .unwrap_or(None);
+
+            vec.push((graph[*a], edge_opt));
+        }
+        else {
+            break;
+        }
+    }
+
+    vec
 }
 
 
 fn get_shortest_path(
     graph: &PersonGraph,
     heritage_map: &HeritageMap,
-    start_id: i32,
-    finish_id: i32
-) -> Result<Vec<i32>, Box<Error>> {
-    let start_idx = heritage_map.get(&start_id).ok_or("invalid start id")?.node_idx;
-    let finish_idx = heritage_map.get(&finish_id).ok_or("invalid finish id")?.node_idx;
+    child_id: i32,
+    ancestor_id: i32
+) -> Result<Vec<PersonRelationship>, Box<Error>> {
+    let start_idx = heritage_map.get(&child_id)
+        .ok_or("invalid start id")?.node_idx;
 
-    let path_opt = astar(&graph, start_idx, |e| e == finish_idx, |_| 2, |_| 0)
-        .map(|(_cost, indices)| {
-            indices.iter().map(|idx| { graph[*idx] }).collect()
-        })
-        .ok_or("no direct or indirect relationship found");
+    let finish_idx = heritage_map.get(&ancestor_id)
+        .ok_or("invalid finish id")?.node_idx;
 
-    Ok(path_opt?)
+    let nodes = astar(&graph, start_idx, |e| e == finish_idx, |_| 1, |_| 0)
+        .map(|(_cost, nodes)| { nodes })
+        .ok_or("no direct or indirect relationship found")?;
+
+    let lookup_name = |person_id| {
+        heritage_map.get(&person_id)
+            .map(|heritage| { heritage.person.Person.clone() })
+            .ok_or("invalid person_id")
+    };
+
+    let mut rels = Vec::new();
+
+    for (person_id, edge_opt) in map_edges(&nodes, &graph) {
+        rels.push(PersonRelationship{
+            id: person_id,
+            name: lookup_name(person_id)?,
+            relationship: edge_opt.map(|edge| {
+                graph.edge_weight(edge).map(|weight| { *weight })
+            }).unwrap_or(None)
+        });
+    }
+
+    Ok(rels)
 }
 
 
-fn fmt_person_id(person_id: i32, heritage_map: &HeritageMap) -> String {
-    let rel = &heritage_map.get(&person_id).unwrap().relationship;
-
-    format!("{}({}) {} -> ", rel.Person, person_id, rel.Beziehung)
-}
-
-
-fn fmt_person_ids(person_ids: &Vec<i32>, heritage_map: &HeritageMap) -> String {
-    person_ids.iter()
-        .map(|person_id| { fmt_person_id(*person_id, &heritage_map) })
+fn fmt_person_relationships(rels: &Vec<PersonRelationship>) -> String {
+    // Imperative style would probably be faster and easier and maintain.
+    rels.iter()
+        .map(|person_relationship| { format!("{}", person_relationship) })
         .collect::<Vec<String>>()
         .join("\n")
 }
 
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "heritage-pathfind", about = "Find relationship path")]
+#[structopt(name = "heritage-pathfind", about = "Find person path")]
 struct CmdInput {
-    #[structopt(short = "c", long = "relationship-csv")]
+    #[structopt(short = "r", long = "relationship-csv")]
     csv_path: String,
-    #[structopt(short = "s", long = "start-id")]
-    start_id: i32,
-    #[structopt(short = "f", long = "finish-id")]
-    finish_id: i32,
+    #[structopt(short = "c", long = "child-id")]
+    child_id: i32,
+    #[structopt(short = "a", long = "ancestor-id")]
+    ancestor_id: i32,
 }
 
 
@@ -177,11 +248,11 @@ fn main() -> Result<(), Box<Error>> {
     get_shortest_path(
         &graph,
         &heritage_map,
-        cmd_input.start_id,
-        cmd_input.finish_id
+        cmd_input.child_id,
+        cmd_input.ancestor_id
     )
-        .map(|person_ids| {
-            println!("{}", fmt_person_ids(&person_ids, &heritage_map));
+        .map(|person_relationships| {
+            println!("{}", fmt_person_relationships(&person_relationships));
             ()
         })
 }
